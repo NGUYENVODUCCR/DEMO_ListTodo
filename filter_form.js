8f5b286ee9d7cc012ed41b9276a3bdb43cbf4e1d
Ducfc2004 <nguyenvoduc2k4@gmai.com>
2023-09-16 12:10:04 +0700

    list-todo

---
+++ filter_form.js
@@ -0,0 +1,15 @@
+function FilterForm(){
+    return(
+        <div className="filterform">
+            <button type ="button" className="btn btn-outline-success">
+            ALL
+            </button>
+            <button type ="button" className="btn btn-outline-success">
+            Done
+            </button>
+            <button type ="button" className="btn btn-outline-success">
+            In-progress
+            </button>
+        </div>
+    );
+}
---
+++ list_todo.js
@@ -0,0 +1,7 @@
+function list_todo(){
+    return (
+        <div className="ListTodo">
+        <h1> TODO LIST</h1>
+        </div>
+    );
+}
